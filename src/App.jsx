9fa1716efb732a0f6038e873b26a545b967f9fd3
Kostiantyn Sukhykh <konstantin.suhih@gmail.com>
2023-07-07 13:45:52 +0300

    create rooms route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Home from './components/home/Home';
+import { Rooms } from './components/rooms/Rooms';
 
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="" element={<MainLayout />}>
+          <Route path="/" element={<MainLayout />}>
             <Route index={true} element={<Home />} />
-            {/* <Route path="rooms" element={<Rooms />} /> */}
+            <Route path="rooms" element={<Rooms />} />
             {/* <Route path="facilities" element={<Facilities />} /> */}
             {/* <Route path="contacts" element={<Contacts />} /> */}
             {/* <Route path="*" element={<NotFound />} /> */}
