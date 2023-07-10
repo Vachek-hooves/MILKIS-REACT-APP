658d593ec99de43175896bdc6964d541741e28ba
Vachek-hooves <vachek0602@gmail.com>
2023-07-10 13:36:59 +0300

    facilities created

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Home from './components/home/Home';
 import { Rooms } from './components/rooms/Rooms';
+import Facilities from './components/facilities/Facilities';
+import Contacts from './components/contacts/Contacts';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<MainLayout />}>
+          <Route path="/hotel/" element={<MainLayout />}>
             <Route index={true} element={<Home />} />
-            <Route path="rooms" element={<Rooms />} />
-            {/* <Route path="facilities" element={<Facilities />} /> */}
-            {/* <Route path="contacts" element={<Contacts />} /> */}
+            <Route path="/hotel/rooms" element={<Rooms />} />
+            <Route path="facilities" element={<Facilities />} />
+            <Route path="contacts" element={<Contacts />} />
             {/* <Route path="*" element={<NotFound />} /> */}
           </Route>
         </Routes>
